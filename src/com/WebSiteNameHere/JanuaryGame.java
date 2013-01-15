457c92cafe13dd0f6c4a700358957281f1628353
Drenguin <drenguino@gmail.com>
2013-01-15 10:58:45 -0500

    Testing EGit

--- src/com/WebSiteNameHere/JanuaryGame.java
+++ src/com/WebSiteNameHere/JanuaryGame.java
@@ -1,38 +1,50 @@
 package com.WebSiteNameHere;
 
-import org.newdawn.slick.Game;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 
-public class JanuaryGame implements Game{
+public class JanuaryGame extends BasicGame{
+	
+	public JanuaryGame()
+	{
+		super("Title");
+	}
+	
+	
+	
+	public static void main(String[] args) throws SlickException
+    {
+         AppGameContainer app = new AppGameContainer(new JanuaryGame());
+ 
+         app.setDisplayMode(800, 600, false);
+         app.start();
+    }
+	
+	
 
 	@Override
-	public boolean closeRequested() {
-		// TODO Auto-generated method stub
-		return false;
+	public void render(GameContainer gc, Graphics g) throws SlickException 
+	{
+		g.setColor(Color.green);
+		g.drawString("Hello World!",200,200);
 	}
 
-	@Override
-	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
+	public void init(GameContainer gc) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
+
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
+	public void update(GameContainer gc, int delta) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
