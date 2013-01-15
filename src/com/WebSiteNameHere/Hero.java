fafe0379783a1508984a8870bbb99feff9d2d238
Drenguin <drenguino@gmail.com>
2013-01-15 11:32:28 -0500

    Hero class would probably be good. This is for our main character.

---
+++ src/com/WebSiteNameHere/Hero.java
@@ -0,0 +1,22 @@
+package com.WebSiteNameHere;
+
+import org.newdawn.slick.Image;
+
+public class Hero extends Image {
+	public int lives;
+	
+	public Hero() {
+		
+	}
+
+	public Hero(Image other) {
+		super(other);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
